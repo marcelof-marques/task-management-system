45e0637dc5f24155932a6ae6c30b2bfd21b42c6b
Marcelo Marques <2200428@ipleiria.pt>
2023-03-02 23:54:14 +0000

    Update ao Program.cs para configurar o CORS (nao permitia pedidos no angular)

--- TaskManagementSystemAPI/TaskManagementSystemAPI/Program.cs
+++ TaskManagementSystemAPI/TaskManagementSystemAPI/Program.cs
@@ -1,6 +1,17 @@
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:4200");
+                      });
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -15,6 +26,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseAuthorization();
 
