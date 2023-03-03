6524a83a5e5822be9e2686946711f7605c19259d
Marcelo Marques <2200428@ipleiria.pt>
2023-03-03 00:02:11 +0000

    Comentários

--- task-app/src/app/components/layout/sidebar/sidebar.component.ts
+++ task-app/src/app/components/layout/sidebar/sidebar.component.ts
@@ -8,15 +8,18 @@ import { Component } from '@angular/core';
 export class SidebarComponent {
 
   toggleSideBar() {
+    // vai buscar a o div da side bar e do botao
     let sidebar = document.querySelector(".sidebar");
     let closeBtn = document.querySelector("#btn");
 
+    // abre ou fecha a side bar
     sidebar.classList.toggle("open");
 
+    // se estiver aberta coloca uns icons se nao coloca outros
     if (sidebar.classList.contains("open")) {
-      closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");//replacing the icons class
+      closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");
     } else {
-      closeBtn.classList.replace("bx-menu-alt-right", "bx-menu");//replacing the icos class
+      closeBtn.classList.replace("bx-menu-alt-right", "bx-menu");
     }
   }
 }
