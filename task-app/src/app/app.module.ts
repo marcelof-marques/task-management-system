7de8ee0fd32dd043d1fdd02eeb5449525c37f1bd
Marcelo Marques <2200428@ipleiria.pt>
2023-03-04 18:41:29 +0000

    Configurações e designs

--- task-app/src/app/app.module.ts
+++ task-app/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './components/layout/sidebar/sidebar.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +14,8 @@ import { SidebarComponent } from './components/layout/sidebar/sidebar.component'
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ task-app/src/app/components/project/project.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ProjectRoutingModule } from './project-routing.module';
+import { ProjectsComponent } from './projects/projects.component';
+
+
+@NgModule({
+  declarations: [
+    ProjectsComponent
+  ],
+  imports: [
+    CommonModule,
+    ProjectRoutingModule
+  ]
+})
+export class ProjectModule { }
