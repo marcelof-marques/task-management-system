c652ab94c727977f85d8882055ae2836a6c6ad09
Marcelo Marques <2200428@ipleiria.pt>
2023-05-21 17:12:01 +0100

    template

---
+++ task-app/src/app/components/project/project-data/project-data.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-project-data',
+    templateUrl: './project-data.component.html',
+    styleUrls: ['./project-data.component.css'],
+})
+export class ProjectDataComponent {}
--- task-app/src/app/components/project/project.module.ts
+++ task-app/src/app/components/project/project.module.ts
@@ -5,9 +5,14 @@ import { ProjectRoutingModule } from './project-routing.module';
 import { ProjectsComponent } from './projects/projects.component';
 import { ProjectContainerComponent } from './project-container/project-container.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ProjectDataComponent } from './project-data/project-data.component';
 
 @NgModule({
-    declarations: [ProjectsComponent, ProjectContainerComponent],
+    declarations: [
+        ProjectsComponent,
+        ProjectContainerComponent,
+        ProjectDataComponent,
+    ],
     imports: [CommonModule, ProjectRoutingModule, NgbModule],
 })
 export class ProjectModule {}
